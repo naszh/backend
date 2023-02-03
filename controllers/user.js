1b198cf796297bf4da236aa1412146539a72f9a4
Naszh <naszh99@gmail.com>
2023-02-04 00:26:17 +0300

    create server side

---
+++ controllers/user.js
@@ -0,0 +1,74 @@
+const UserService = require('../service/user');
+const { validationResult } = require('express-validator');
+const ApiError = require('../exceptions/error');
+
+class UserController {
+  async registration(req, res, next) {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequest('Validation error', errors.array()));
+      }
+      const { email, password } = req.body;
+      const userData = await UserService.registration(email, password); // {token, userData}
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+
+      return res.json(userData);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const userData = await UserService.login(email, password);
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      return res.json(userData);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+      const { refreshToken } = req.cookies;
+      const token = await UserService.logout(refreshToken);
+      res.clearCookie('refreshToken');
+      return res.json(token);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async refresh(req, res, next) {
+    try {
+      const { refreshToken } = req.cookies;
+      const userData = await UserService.refresh(refreshToken);
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      return res.json(userData);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getUsers(req, res) {
+    try {
+      const users = await UserService.getAllUsers();
+      return res.json(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = new UserController();
